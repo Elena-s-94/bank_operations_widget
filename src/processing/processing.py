3c738faf60496bf4f931fe4611e6af9474695a35
Elena <elenka.sazonova.94@mail.ru>
2026-07-05 17:03:43 +0300

    Implement filter_by_state and sort_by_date functions

--- src/processing/processing.py
+++ src/processing/processing.py
@@ -12,3 +12,21 @@ def filter_by_state(data_list: List[Dict], state: str = 'EXECUTED') -> List[Dict
         List[Dict]: Новый список словарей с указанным состоянием.
     """
     return [item for item in data_list if item.get('state') == state]
+
+
+
+
+from datetime import datetime
+
+def sort_by_date(data_list: List[Dict], descending: bool = True) -> List[Dict]:
+    """
+    Сортирует список словарей по дате.
+
+    Параметры:
+        data_list (List[Dict]): Список словарей с данными о транзакциях.
+        descending (bool): Порядок сортировки. По умолчанию - True (по убыванию).
+
+    Возвращает:
+        List[Dict]: Новый список словарей, отсортированных по дате.
+    """
+    return sorted(data_list, key=lambda x: datetime.fromisoformat(x['date']), reverse=descending)
